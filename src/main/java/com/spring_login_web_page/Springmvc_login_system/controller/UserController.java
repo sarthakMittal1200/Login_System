56dcb5686441bad67dcc0b19b8d96c280e1a4eaf
sarthakMittal1200 <sarthak3350@gmail.com>
2025-10-27 11:19:19 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/UserController.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/controller/UserController.java
@@ -1,36 +1,28 @@
 package com.spring_login_web_page.Springmvc_login_system.controller;
-
+import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
 import com.spring_login_web_page.springmvc_login_system.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*")
-
-
+@Controller
 public class UserController {
     @Autowired
-    public UserServices UserService;
-    @GetMapping("/User/{Password}")
-    public String getAllUserPassword(@RequestParam String Password) {
-        return UserService.getAllUserPassword(Password);
+    public  UserServices userServices;
+    @GetMapping("/user")
+    public String AccessUserPage() {return "User Page Is Successfully Accessed";}
+   @GetMapping("/saveAll/{Authorities]")
+    public String getAllBYAuthorities(@RequestParam List<SecurityProperties.User> Authorities, @RequestBody ApplicationUser applicationUser){
+        return  userServices.getAllBYAuthorities(Authorities);
     }
-    @GetMapping("/User/{username}")
-    public String getAllUsername(@RequestParam String username) {
-        return UserService.getAllUsername(username);
+    @GetMapping("/saveAll/{Password}")
+    public String getAllByPassword(String Password){
+        return userServices.getAllByPassword(Password);
     }
-    @GetMapping("/user/req/login")
-    public String AccessUserPage() {return "Login Page Is Successfully Accessed By User";}
-    @GetMapping("/user/req/register")
-    public String RegisterAccessUserPage() {return "Register Page Is Successfully Accessed By User";}
-    @GetMapping("/user/req/signup")
-    public String SignupAccessUserPage() {return "Signup Page Is Successfully Accessed By User";}
-
 }
