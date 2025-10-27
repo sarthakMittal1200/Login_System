6f53e072c1545302b61b6c8b156338f83a7f6941
sarthakMittal1200 <sarthak3350@gmail.com>
2025-10-27 11:18:56 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/service/UserServices.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/service/UserServices.java
@@ -1,61 +1,53 @@
-// package com.spring_login_web_page.springmvc_login_system.service;
+package com.spring_login_web_page.springmvc_login_system.service;
 
-// import com.spring_login_web_page.Springmvc_login_system.controller.UserController;
-// import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
-// import com.spring_login_web_page.springmvc_login_system.repository.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
+import com.spring_login_web_page.springmvc_login_system.repository.UserRepository;
+import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-// import javax.management.relation.Role;
-// import java.beans.Encoder;
-// import java.util.Base64;
-// import java.util.HashSet;
-// import java.util.Set;
-
-
-// import static org.apache.el.lang.ELArithmetic.add;
-
-// @Service
-// public class UserServices implements UserDetailsService {
-//     @Autowired
-//     public User user; 
-
-//    @Autowired
-//     private PasswordEncoder passwordEncoder;
-
-//     @Autowired
-//     private ApplicationUser applicationUser;
-
-//     @Autowired
-//     private  UserRepository userRepository;
-
-//     userRepository.findByUsername();
-//     if(user.isPresent()){
-//         user=user.get();
-//         return user.builder()
-//                 .username(UserController.getUsername())
-//                 .username(UserController.getPassword())
-//                 .build();
-                
-//     }
+import javax.management.relation.Role;
+import java.beans.Encoder;
+import java.util.Base64;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
-//     @Autowired
-//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//         throw new UsernameNotFoundException(username+ "LoadUserBYUserName");
-//         System.out.println(UserDetailsService.getName());
-//         if(!username.equals("Sarthak"))throw new UsernameNotFoundException(username+ "LoadUserByUsername");
-//         roles=new Role(1,"USER");
-//         return new ApplicationUser(1,"SARTHAK", "PASSWORD",roles);
-//     }
-//     Set<Role> roles=new HashSet<>();
-// }
-// //    @Override
-// //   UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-// //        return null;
-// //    }
+import static org.apache.el.lang.ELArithmetic.add;
 
+@Service
+public class UserServices implements UserDetailsService {
+   @Autowired
+   public UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private ApplicationUser applicationUser;
+
+    public String getAllByPassword(String Password){
+        return userRepository.saveAllByPassword(Password);
+    }
+//    @Override
+@Autowired
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        throw new UsernameNotFoundException(username+ "LoadUserBYUserName");
+        System.out.println(UserDetailsService.getName());
+        if(!username.equals("Sarthak"))throw new UsernameNotFoundException(username+ "LoadUserByUsername");
+        roles=new Role(1,"USER");
+        return new ApplicationUser(1,"SARTHAK", "PASSWORD",roles);
+    }
+    public String getAllBYAuthorities(List<SecurityProperties.User> Authorities){
+    return  userRepository.saveAllBYAuthorities(Authorities);
+    }
+    Set<Role> roles=new HashSet<>();
+
+//    @Override
+//   UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return null;
+//    }
+}
