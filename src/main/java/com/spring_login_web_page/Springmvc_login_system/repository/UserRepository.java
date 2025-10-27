e463f77ab460aba7939d3523ac3a2272d351e135
sarthakMittal1200 <sarthak3350@gmail.com>
2025-10-27 11:15:30 +0530

    Add files via upload

--- src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
+++ src/main/java/com/spring_login_web_page/Springmvc_login_system/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.spring_login_web_page.springmvc_login_system.repository;
 
 import com.spring_login_web_page.springmvc_login_system.model.ApplicationUser;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,10 +10,11 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, String> {
-    public String findByUsername(String username);
-    public String save();
-    public List<ApplicationUser> getAllUserId(Integer UserId);
-    public List<ApplicationUser>getAllUsername(String UserName);
-    public String getAllUserByPassword(String userPassword);
+    public List<ApplicationUser> getUserId(Integer UserId);
     public List<ApplicationUser>setUserId(Integer UserId);
+    static Optional<ApplicationUser> findByUsername(String username);
+    public String saveAllBYAuthorities(List<SecurityProperties.User>Authorities);
+    public List<ApplicationUser>getUsername(String UserName);
+    public String saveAllByPassword(String Password);
+
 }
